4fc19c69bcb92e2e6edf06ce95e60ff1ca60d078
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-11 20:53:16 -0700

    fix: ESLint error

--- app/page.tsx
+++ app/page.tsx
@@ -25,80 +25,80 @@ const GameListPage = async () => {
     return (
         <>
 
-        <div className="relative">
-            {/* Game Form */}
-            <div className="mb-4">
-                <GameForm />
-            </div>
+            <div className="relative">
+                {/* Game Form */}
+                <div className="mb-4">
+                    <GameForm />
+                </div>
 
-            {/* Game List */}
-            <div>
-                {games.length === 0 ? (
-                    <div>You don't have any game history</div>
+                {/* Game List */}
+                <div>
+                    {games.length === 0 ? (
+                        <div>You don&apos;t have any game history</div>
                 ) : (
-                    <div className="space-y-2">
-                        {games.map((game) => (
-                            <div
-                                key={game.id}
-                                className="flex items-center justify-between p-4 rounded-lg shadow-lg bg-base-300"
-                            >
-                                {/* Game Name */}
-                                <h1 className="text-xl font-semibold w-1/6 truncate">
-                                    {game.game_name}
-                                </h1>
+                <div className="space-y-2">
+                    {games.map((game) => (
+                        <div
+                            key={game.id}
+                            className="flex items-center justify-between p-4 rounded-lg shadow-lg bg-base-300"
+                        >
+                            {/* Game Name */}
+                            <h1 className="text-xl font-semibold w-1/6 truncate">
+                                {game.game_name}
+                            </h1>
 
-                                {/* Players' Information */}
-                                <div className="space-x-2 w-2/6 hidden lg:flex lg:gap-1">
-                                    {["player1_name", "player2_name", "player3_name", "player4_name"].map(
-                                        (player, index) => (
-                                            <div
-                                                key={index}
-                                                className="flex flex-col items-center"
-                                            >
-                                                <div className="avatar placeholder">
-                                                    <div className="bg-neutral text-neutral-content w-12 h-12 rounded-md ring-neutral ring-offset-base-100 ring ring-offset-2 flex items-center justify-center">
-                                                        <span className="text-lg">
-                                                            {game[player].substring(0, 2)}
-                                                        </span>
-                                                    </div>
+                            {/* Players' Information */}
+                            <div className="space-x-2 w-2/6 hidden lg:flex lg:gap-1">
+                                {["player1_name", "player2_name", "player3_name", "player4_name"].map(
+                                    (player, index) => (
+                                        <div
+                                            key={index}
+                                            className="flex flex-col items-center"
+                                        >
+                                            <div className="avatar placeholder">
+                                                <div className="bg-neutral text-neutral-content w-12 h-12 rounded-md ring-neutral ring-offset-base-100 ring ring-offset-2 flex items-center justify-center">
+                                                    <span className="text-lg">
+                                                        {game[player].substring(0, 2)}
+                                                    </span>
                                                 </div>
                                             </div>
-                                        )
-                                    )}
-                                </div>
-
-                                {/* Game Creation Time */}
-                                <p className="text-center text-sm w-1/6 hidden sm:block">
-                                    {new Date(game.created_at).toLocaleString()}
-                                </p>
-
-                                {/* Action Buttons */}
-                                <div className="flex space-x-2 w-1/6 justify-end">
-                                    <Link
-                                        href={`/game/${game.id}`}
-                                        className="btn btn-success btn-sm"
-                                    >
-                                        Track
-                                    </Link>
-
-                                    <EditGame game={game} />
-
-                                    <form action={deleteGame}>
-                                        <input type="hidden" name="id" value={game.id} />
-                                        <button
-                                            type="submit"
-                                            className="btn btn-sm btn-error"
-                                        >
-                                            Delete
-                                        </button>
-                                    </form>
-                                </div>
+                                        </div>
+                                    )
+                                )}
                             </div>
-                        ))}
-                    </div>
+
+                            {/* Game Creation Time */}
+                            <p className="text-center text-sm w-1/6 hidden sm:block">
+                                {new Date(game.created_at).toLocaleString()}
+                            </p>
+
+                            {/* Action Buttons */}
+                            <div className="flex space-x-2 w-1/6 justify-end">
+                                <Link
+                                    href={`/game/${game.id}`}
+                                    className="btn btn-success btn-sm"
+                                >
+                                    Track
+                                </Link>
+
+                                <EditGame game={game} />
+
+                                <form action={deleteGame}>
+                                    <input type="hidden" name="id" value={game.id} />
+                                    <button
+                                        type="submit"
+                                        className="btn btn-sm btn-error"
+                                    >
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    ))}
+                </div>
                 )}
             </div>
-        </div>
+        </div >
         </>
     )
 }
