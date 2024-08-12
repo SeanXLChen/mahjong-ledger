2bd65ad992a13b2ab9be2268d5efc56967f8ecd8
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-11 20:05:22 -0700

    EditGame and GameForm use similar light design

--- app/components/EditGame.tsx
+++ app/components/EditGame.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useState } from "react"
 import { updateGame } from "../server-action/updateGame"
 
@@ -14,7 +13,7 @@ type Game = {
     game_name: string
 }
 
-export default function EditGame({game}: {game: Game}) {
+export default function EditGame({ game }: { game: Game }) {
 
     const [showModal, setShowModal] = useState(false)
     const [formData, setFormData] = useState({
@@ -26,12 +25,12 @@ export default function EditGame({game}: {game: Game}) {
         player4: game.player4_name
     })
 
-    const handleChange = (e) => setFormData({...formData, [e.target.name]: e.target.value})
+    const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
 
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+        event.preventDefault(); // Prevent the default form submission behavior
         setLoading(true);
 
         const formData = new FormData(event.currentTarget);
@@ -40,10 +39,11 @@ export default function EditGame({game}: {game: Game}) {
         setLoading(false);
 
         if (response && response.message) {
-            alert(response.message);
         } else {
             alert("An error occurred. Please try again.");
         }
+
+        setShowModal(false); // Close the modal after submission
     };
 
     return (
@@ -52,80 +52,85 @@ export default function EditGame({game}: {game: Game}) {
                 Edit
             </button>
             {showModal && (
-                <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center px-4">
-                    <div className="modal-content bg-gray-900 p-6 rounded-lg w-full max-w-md">
-                    <span className="close text-white text-xl leading-none hover:text-gray-300 cursor-pointer float-right" onClick={() => setShowModal(false)}>&times;</span>
-                    <form action={updateGame} onSubmit={() => setShowModal(false)} className="mt-4">
-                        <input 
-                            type="hidden" 
-                            name="id" 
-                            value={game.id} 
-                        />
-                        
-                        <div className="mb-4">
-                            <label htmlFor="player1" className="block text-gray-300 mb-2">Player 1</label>
-                            <input 
-                                type="text" 
-                                id="player1"
-                                name="player1" 
-                                value={formData.player1} 
-                                onChange={handleChange}
-                                className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500" 
+                <div className="fixed inset-0 bg-gray-600 bg-opacity-75 flex justify-center items-center z-[100]">
+                    <div className="bg-base-100 p-6 rounded-lg w-full max-w-md relative">
+                        <span
+                            className="close text-xl leading-none hover:text-gray-300 cursor-pointer float-right"
+                            onClick={() => setShowModal(false)}
+                        >
+                            &times;
+                        </span>
+                        <form onSubmit={handleSubmit} className="mt-4">
+                            <input
+                                type="hidden"
+                                name="id"
+                                value={game.id}
                             />
-                        </div>
 
-                        <div className="mb-4">
-                            <label htmlFor="player2" className="block text-gray-300 mb-2">Player 2</label>
-                            <input 
-                                type="text" 
-                                id="player2"
-                                name="player2" 
-                                value={formData.player2} 
-                                onChange={handleChange}
-                                className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500" 
-                            />
-                        </div>
+                            <div className="mb-4">
+                                <label htmlFor="player1" className="block text-gray-700">Player 1</label>
+                                <input
+                                    type="text"
+                                    id="player1"
+                                    name="player1"
+                                    value={formData.player1}
+                                    onChange={handleChange}
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
 
-                        <div className="mb-4">
-                            <label htmlFor="player3" className="block text-gray-300 mb-2">Player 3</label>
-                            <input 
-                                type="text" 
-                                id="player3"
-                                name="player3" 
-                                value={formData.player3} 
-                                onChange={handleChange}
-                                className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500" 
-                            />
-                        </div>
+                            <div className="mb-4">
+                                <label htmlFor="player2" className="block text-gray-700">Player 2</label>
+                                <input
+                                    type="text"
+                                    id="player2"
+                                    name="player2"
+                                    value={formData.player2}
+                                    onChange={handleChange}
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
 
-                        <div className="mb-4">
-                            <label htmlFor="player4" className="block text-gray-300 mb-2">Player 4</label>
-                            <input 
-                                type="text" 
-                                id="player4"
-                                name="player4" 
-                                value={formData.player4} 
-                                onChange={handleChange}
-                                className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500" 
-                            />
-                        </div>
+                            <div className="mb-4">
+                                <label htmlFor="player3" className="block text-gray-700">Player 3</label>
+                                <input
+                                    type="text"
+                                    id="player3"
+                                    name="player3"
+                                    value={formData.player3}
+                                    onChange={handleChange}
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
 
-                        <div className="mb-4">
-                            <label htmlFor="gameName" className="block text-gray-300 mb-2">Game Name</label>
-                            <input 
-                                type="text" 
-                                id="gameName"
-                                name="gameName" 
-                                value={formData.gameName} 
-                                onChange={handleChange}
-                                className="w-full p-2 rounded bg-gray-800 text-white border border-gray-700 focus:border-blue-500" 
-                            />
-                        </div>
-                        
-                        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Update Game
-                        </button>
-                    </form>
+                            <div className="mb-4">
+                                <label htmlFor="player4" className="block text-gray-700">Player 4</label>
+                                <input
+                                    type="text"
+                                    id="player4"
+                                    name="player4"
+                                    value={formData.player4}
+                                    onChange={handleChange}
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
+
+                            <div className="mb-4">
+                                <label htmlFor="gameName" className="block text-gray-700">Game Name</label>
+                                <input
+                                    type="text"
+                                    id="gameName"
+                                    name="gameName"
+                                    value={formData.gameName}
+                                    onChange={handleChange}
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
+
+                            <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Update Game
+                            </button>
+                        </form>
                     </div>
                 </div>
             )}
--- app/components/GameForm.tsx
+++ app/components/GameForm.tsx
@@ -13,45 +13,45 @@ export default function GameForm() {
                 </button>
             </div>
             {showModal && (
-                <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center px-4">
-                    <div className="modal-content bg-gray-900 p-6 rounded-lg w-full max-w-md">
+                <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center px-4 z-[100]">
+                    <div className="bg-base-100 p-6 rounded-lg w-full max-w-md relative">
                         <span
-                            className="close text-white text-xl leading-none hover:text-gray-300 cursor-pointer float-right"
+                            className="close text-xl leading-none hover:text-gray-300 cursor-pointer float-right"
                             onClick={() => setShowModal(false)}
                         >
                             &times;
                         </span>
                         <form action={addGame} className="mt-4 flex flex-col items-center">
-                            <div className="p-2 mx-2 flex justify-items-center">
+                            <div className="p-2 mx-2 flex justify-items-center w-full">
                                 <div className="flex mb-4 w-full">
                                     <input
                                         type="text"
                                         placeholder="Game Name"
                                         id="gameName"
                                         name="gameName"
-                                        className="shadow appearance-none border border-gray-600 bg-gray-700 rounded w-full py-2 px-3 text-white"
+                                        className="input input-bordered w-full"
                                         required
                                     />
                                 </div>
                             </div>
 
-                            <div className="flex flex-wrap gap-2 justify-items-center">
+                            <div className="flex flex-wrap gap-2 justify-items-center w-full">
                                 {["player1", "player2", "player3", "player4"].map((player, index) => (
-                                    <div key={index} className="flex mb-4 w-full">
+                                    <div key={index} className="flex mb-4 mx-2 w-full">
                                         <input
                                             type="text"
                                             placeholder={`${player.charAt(0).toUpperCase() + player.slice(1)} name`}
                                             id={player}
                                             name={player}
-                                            className="shadow appearance-none border border-gray-600 bg-gray-700 rounded w-full py-2 px-3 text-white"
+                                            className="input input-bordered w-full"
                                             required
                                         />
                                     </div>
                                 ))}
                             </div>
 
-                            <div className="w-full">
-                                <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <div className="w-full px-2">
+                                <button type="submit" className="btn btn-primary w-full">
                                     Start Game
                                 </button>
                             </div>
